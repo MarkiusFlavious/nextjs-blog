6312d9d01369b4991600e26d0019d70f68f984f2
Mark Hurlin <mark.s.hurlin@gmail.com>
2025-03-18 17:51:08 +0200

    Hopefully Fixed

--- pages/components/FeaturedImage.js
+++ pages/components/FeaturedImage.js
@@ -2,8 +2,6 @@ import Image from "next/image";
 import Link from "next/link";
 
 export default function FeaturedImage({ post }) {
-    console.log("FeaturedImage: Received post:", post);
-
     let img = {
         src: "https://headless.digitalhumanitydev.co.za/wp-content/uploads/2025/03/post-feature-9.webp",
         width: 300,
@@ -17,6 +15,18 @@ export default function FeaturedImage({ post }) {
             width: mediaDetails.width,
             height: mediaDetails.height,
         };
+    }
+
+    if (!post) {
+      return (
+        <Image
+          src={img.src}
+          width={img.width}
+          height={img.height}
+          alt={"default image"}
+          className="h-full object-cover rounded-xl"
+        />
+      );
     }
 
     return (
